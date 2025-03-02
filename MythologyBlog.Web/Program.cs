fa380237cf999aefcd7a17b17993fdc644f47991
Alex <bromilowalex@aol.com>
2025-03-02 18:01:26 +0000

    initial commit

---
+++ MythologyBlog.Web/Program.cs
@@ -0,0 +1,37 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+
+config.AddJsonFile("connectionstrings.json", optional: true);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
